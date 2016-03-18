bf4a4fc79f49733c7baaf857bd8f5d9a03a40aa8
cassicakes <aguiar.cas@gmail.com>
2016-03-18 09:10:22 -0700

    basic funcitoning with out winner alert.

--- starter-code/js/app.js
+++ starter-code/js/app.js
@@ -1,6 +1,57 @@
 /* setup your angular app here */
+var app = angular.module('FruitVeg', []); 
+
+app.controller('FruitVegCtlr', ['$scope', function($scope) { //I need a better explanation of what all this is. esp scope in [0] above. 
+  $scope.pool = fruits.concat(vegetables); //also need to suffle @each refresh still
+  $scope.fruit = [];
+  $scope.veggies = [];
+
+  $scope.moveToFruit = function(idx) {
+    $scope.fruit.push($scope.pool[idx]);
+    $scope.pool.splice(idx,1); 
+  };
+
+  $scope.moveToVeggie = function(idx) {
+    $scope.veggies.push($scope.pool[idx]);
+    $scope.pool.splice(idx,1);
+  };
+
+  $scope.veggieToPool = function(idx) {
+    $scope.pool.push($scope.veggies[idx]);
+    $scope.veggies.splice(idx,1);
+  };
+
+  $scope.fruitToPool = function(idx) {
+    $scope.pool.push($scope.fruit[idx]);
+    $scope.fruit.splice(idx,1);
+  };
+
+  $scope.turnRed = function(type, idx) {
+    if ($scope.pool.length == 0) {
+      if (type == "veg") {
+        if (vegetables.indexOf($scope.veggies[idx]) === -1) { //if item doesn't exist in array it will return -1, hence why it is there. 
+          return true;
+        } else {
+          return false;
+        }
+      }
+      if (type == "fruit") {
+        if (fruits.indexOf($scope.fruit[idx]) === -1) {
+          return true;
+        } else {
+          return false;
+        }
+      }
+    }
+  };
+//this is the area that I couldn't figure out... I know that I need to sent an altert saying that you've won, but the funcitoning, not sure. 
+  $scope.win = function() {
+
+  }
+  
+}]);
 
 //debug stuff to show the app is loading and fruit / veggies are available
 console.log('App Started');
-console.log('Fruit count', fruit.length);
+console.log('Fruits count', fruits.length);
 console.log('Veggie count', vegetables.length);
